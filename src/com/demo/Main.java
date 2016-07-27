75b9c31eb7887b69dbd2d927a4a3c1a94e127ffa
Junli <jlli@thoughtworks.com>
2016-07-27 23:41:36 +0800

    Develop: Added the method of darwing triangle

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Triangle triangle = new Triangle();
 
-        new Triangle().printSingleAsterisk();
+        triangle.printSingleAsterisk();
+        triangle.drawHorizontalLine(8);
+        triangle.drawVerticalLine(3);
+        triangle.drawRightTriangle(3);
     }
 }
--- src/com/demo/Triangle.java
+++ src/com/demo/Triangle.java
@@ -1,10 +1,31 @@
 package com.demo;
 
+import java.util.concurrent.SynchronousQueue;
+import java.util.stream.IntStream;
+
 /**
  * Created by jlli on 7/27/16.
  */
 public class Triangle {
+
     public void printSingleAsterisk(){
         System.out.println('*');
+    }
+
+    public void drawHorizontalLine(int n){
+        IntStream.range(0, n).forEach(
+                i -> System.out.print('*')
+        );
+        System.out.println();
+    }
+
+    public void drawVerticalLine(int n){
+        IntStream.range(0, n).forEach(
+                i -> System.out.println('*')
+        );
+    }
+
+    public void drawRightTriangle(int n){
+
     }
 }
